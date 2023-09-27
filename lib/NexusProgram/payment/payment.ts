2b4f32f59b6822254fef92f3d61e72e95e0b3c4b
Xmohamed1X <mohamedekhadel00@gmail.com>
2023-09-27 15:52:31 +0100

    init program

---
+++ lib/NexusProgram/payment/payment.ts
@@ -0,0 +1,81 @@
+import {
+    AnchorProvider, BN, Program, utils, Wallet, web3
+} from '@project-serum/anchor';
+import { IDENTIFIER_PREFIX, NEXUS_PREFIX, USER_PREFIX } from "../../constants/constants";
+const idl = require("../../../data/nexus.json")
+
+export async function one_time_payment(
+    anchorWallet: any,
+    connection: web3.Connection,
+    user: web3.PublicKey,
+) {
+
+    const provider = new AnchorProvider(
+        connection, anchorWallet, {"preflightCommitment": "processed"},
+        );
+
+    const PROGRAM_ID = new web3.PublicKey(idl.metadata.address)
+    const program = new Program(idl, idl.metadata.address, provider);
+    // const twitter_id_resp = await fetch("/api/twitter/get_session");
+    // const twitter_id = (await twitter_id_resp.json()).twitter_id;
+
+    const [identifier] = web3.PublicKey.findProgramAddressSync(
+        [
+            Buffer.from(IDENTIFIER_PREFIX),
+        ],
+        PROGRAM_ID
+    );
+
+    const account = await program.account.identifier.fetch(identifier, "confirmed");
+
+    const [payment, bump] = web3.PublicKey.findProgramAddressSync(
+        [
+            anchorWallet.publicKey.toBuffer(),
+            new BN(Number(account.count)).toArrayLike(Buffer, "le", 8),
+        ],
+        PROGRAM_ID
+        );
+        
+        
+        const [founder] = web3.PublicKey.findProgramAddressSync(
+            [
+            Buffer.from(USER_PREFIX),
+            anchorWallet.publicKey.toBuffer(),
+            ],
+            PROGRAM_ID
+      );
+      
+      const [reciever] = web3.PublicKey.findProgramAddressSync(
+          [
+            Buffer.from(USER_PREFIX),
+            user.toBuffer(),
+          ],
+          PROGRAM_ID
+    );
+    
+    const [escrow] = web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from(NEXUS_PREFIX),
+        ],
+        PROGRAM_ID
+  );
+
+
+
+    console.log("init payment");
+
+        const tx = await program.methods.payment().accounts({
+            payment: payment,
+            founder: founder,
+            reciever: reciever,
+            address: user,
+            authority: anchorWallet.publicKey,
+            escrow: escrow,
+            systemProgram: web3.SystemProgram.programId
+        })
+        .transaction()
+        // .rpc({
+        //     commitment: "confirmed",
+        // })
+    return tx;
+    }
---
+++ lib/NexusProgram/user/update_user.ts
@@ -0,0 +1,71 @@
+import {
+    AnchorProvider,
+    BN,
+    Program, web3
+} from '@project-serum/anchor';
+import { USER_PREFIX } from "../../constants/constants";
+const idl = require("../../../data/staking.json")
+
+/**
+    name: String,
+    image: String,
+    category: String,
+    roles: String,
+    level_of_expertise: String,
+    payment_rate_per_hour: u64,
+    profile_overview: String,
+    links: String,
+    nigotion: Boolean,
+ */
+
+export async function update_user(
+    anchorWallet: any,
+    connection: web3.Connection,
+    name: string,
+    image: string,
+    category: string,
+    roles: string,
+    level_of_expertise: string,
+    links: string,
+    profile_overview: string,
+    payment_rate_per_hour: number,
+    nigotion: boolean
+) {
+
+    const provider = new AnchorProvider(
+        connection, anchorWallet, {"preflightCommitment": "processed"},
+        );
+
+    const PROGRAM_ID = new web3.PublicKey(idl.metadata.address)
+    const program = new Program(idl, idl.metadata.address, provider);
+
+    const [user] = web3.PublicKey.findProgramAddressSync(
+        [
+            anchorWallet.publicKey.toBuffer(),
+            Buffer.from(USER_PREFIX),
+        ],
+        PROGRAM_ID
+    );
+
+        const tx = await program.methods.updateUser({      
+                name: name,
+                image: image,
+                category: category,
+                roles: roles,
+                level_of_expertise: level_of_expertise,
+                links: links,
+                profile_overview: profile_overview,
+                payment_rate_per_hour: payment_rate_per_hour,
+                nigotion: nigotion,
+            }).accounts({
+                user: user,
+                authority: anchorWallet.publicKey,
+                systemProgram: web3.SystemProgram.programId
+        })
+        // .transaction()
+        .rpc({
+            commitment: "confirmed",
+        })
+
+    return tx;
+    }
