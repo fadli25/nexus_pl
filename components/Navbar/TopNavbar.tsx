651bfc8b3112838966579408ced19ad0012582d9
Xmohamed1X <mohamedekhadel00@gmail.com>
2023-10-06 20:19:55 +0100

    init program

--- components/Navbar/TopNavbar.tsx
+++ components/Navbar/TopNavbar.tsx
@@ -1,12 +1,13 @@
-import React, { ReactNode, useState } from "react";
 import { Button, IconButton } from "@mui/material";
-import { IoNotifications, IoSettingsOutline } from "react-icons/io5";
-import { BsFillChatFill } from "react-icons/bs";
-import { MdLocalGroceryStore } from "react-icons/md";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { ReactNode, useState } from "react";
 import { BiMenu, BiMenuAltRight } from "react-icons/bi";
+import { BsFillChatFill } from "react-icons/bs";
+import { IoNotifications, IoSettingsOutline } from "react-icons/io5";
+import { MdLocalGroceryStore } from "react-icons/md";
 import LeftNavbar from "./LeftNavbar";
 
 export default function TopNavbar() {
@@ -299,7 +300,8 @@ export default function TopNavbar() {
               >
                 Rebirth Tower
               </motion.button>
-              <Button
+              <WalletMultiButton/>
+              {/* <Button
                 variant="contained"
                 sx={{
                   fontSite: "0.7vw",
@@ -320,7 +322,7 @@ export default function TopNavbar() {
                 }}
               >
                 Connet Wallet
-              </Button>
+              </Button> */}
               <motion.button
                 onClick={() => setShowLeftNavbar(!showLeftNavbar)}
                 whileHover={{ scale: 1.04 }}
---
+++ lib/NexusProgram/user/utils/user_info.ts
@@ -0,0 +1,30 @@
+import { AnchorProvider, Program, web3 } from '@project-serum/anchor';
+import { USER_PREFIX } from "../../../constants/constants";
+const idl = require("../../../../data/nexus.json")
+
+export async function get_user_info(
+    anchorWallet: any,
+    connection: web3.Connection,
+) {
+    try {
+        const provider = new AnchorProvider(
+            connection, anchorWallet, {"preflightCommitment": "processed"},
+            );
+        const PROGRAM_ID = new web3.PublicKey(idl.metadata.address)
+
+        const [user] = web3.PublicKey.findProgramAddressSync(
+            [
+                anchorWallet.publicKey.toBuffer(),
+                Buffer.from(USER_PREFIX),
+            ],
+            PROGRAM_ID
+        );
+
+        const program = new Program(idl, idl.metadata.address, provider);
+        const account = await program.account.user.fetchNullable(user);
+        console.log(account);
+        return account;
+    } catch(e) {
+        console.log(e);
+    }
+}
