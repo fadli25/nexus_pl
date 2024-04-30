389af8bd41746efa0d1dd2923effbff708b86449
Xmohamed1X <mohamedekhadel00@gmail.com>
2024-04-30 19:00:37 +0100

    done

--- components/mint/CardMintBoxFloor.tsx
+++ components/mint/CardMintBoxFloor.tsx
@@ -5,7 +5,7 @@ export default function CardMintBox({ children }: any) {
   return (
     <div className="w-full border-[0.12vw] border-white rounded-[0.4vw] p-[5vw] md:p-[2vw]">
       <div className="flex justify-end">
-        <CountdownTimer time={"2024-04-30T18:00:00"} />
+        Live
       </div>
       {children}
     </div>
